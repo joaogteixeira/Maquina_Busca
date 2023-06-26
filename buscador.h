a32f9f04af6abe8334906e05305d24dfd286946a
joaogteixeira <117311085+joaogteixeira@users.noreply.github.com>
2023-06-26 15:09:23 -0300

    Create buscador.h
    o inicio do arquivo H

---
+++ buscador.h
@@ -0,0 +1,14 @@
+#ifndef __BUSCADOR_H__
+#define __BUSCADOR_H__
+
+#include <algorithm>
+#include <fstream>
+#include <list>
+#include <map>
+#include <string>
+#include <unordered_map>
+
+using std::list;
+using std::map;
+using std::string;
+using std::unordered_map;
